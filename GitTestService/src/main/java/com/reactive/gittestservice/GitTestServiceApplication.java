66374bf57b24b1e451dfc43327919bdfb6629e36
dzamir <dzamir@perrigo.com>
2022-01-26 10:48:12 +0200

    first test commit git-service

---
+++ GitTestService/src/main/java/com/reactive/gittestservice/GitTestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.gittestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTestServiceApplication.class, args);
+	}
+
+}
---
+++ GitTestService/src/test/java/com/reactive/gittestservice/GitTestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.gittestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
